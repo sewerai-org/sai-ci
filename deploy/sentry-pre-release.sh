82da326245eb104fc58e3442de374844a0743668
Marty Berryman <mqarty@sewerai.com>
2019-10-13 14:40:15 -0700

    Use env var to get the SENTRY PROJECT name

--- deploy/sentry-pre-release.sh
+++ deploy/sentry-pre-release.sh
@@ -6,7 +6,7 @@
 VERSION=$(sentry-cli releases propose-version) 
 
 # Create a release
-sentry-cli releases new -p cortland $VERSION
+sentry-cli releases new -p $SENTRY_PROJECT $VERSION
 
 # Associate commits with the release
 sentry-cli releases set-commits --auto $VERSION
