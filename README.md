3ae14081838d90bdae6727e978b4f542bc8d12a0
Marty Berryman <mqarty@sewerai.com>
2019-10-17 10:28:20 -0700

    skip ci: squash yml loader fixes, python3.8->3.7

--- README.md
+++ README.md
@@ -1,11 +1,8 @@
 # docker-firebase
 
-Docker image containing python, firebase-admin, firebase-tools, and firebase-functions.
+Docker image containing python2, firebase-admin, firebase-tools, and firebase-functions.
 
 Used for easily deploying functions via CI.
 
 Built via CircleCI.
 
-# docker hub
-
-https://hub.docker.com/r/mqarty/docker-firebase/
--- deploy/sls_plugin_installer.py
+++ deploy/sls_plugin_installer.py
@@ -1,9 +1,13 @@
 import subprocess
 import yaml
 
+# Ignore ! in yaml. This is only looking for plugins
+# and not trying to parse the entire document.
+yaml.add_multi_constructor('!', lambda loader, suffix, node: None)
+
 plugins = None
 with open("serverless.yml", "r+") as f:
-    yml = yaml.load(f, Loader=yaml.FullLoader)
+    yml = yaml.load(f, Loader=yaml.Loader)
     plugins = yml['plugins']
 
 for plugin in plugins:
